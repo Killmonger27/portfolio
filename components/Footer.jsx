c970cf6ad7e459e8d7357724b6a23e65be8c0f5b
Killmonger27 <landryouarma45@gmail.com>
2025-02-03 22:24:26 +0000

    Ajout des fichiers de conf de Webstorm et mise en forme du footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer>Footer</footer>
-  )
-}
+      <footer className="bg-gray-800 text-white py-6 mt-12">
+        <div className="container mx-auto text-center">
+          <p>© 2023 Mon Portfolio. Tous droits réservés.</p>
+          <div className="mt-4 flex justify-center space-x-4">
+            <a href="https://github.com/ton-utilisateur" className="hover:text-gray-400">
+              GitHub
+            </a>
+            <a href="https://linkedin.com/in/ton-profil" className="hover:text-gray-400">
+              LinkedIn
+            </a>
+          </div>
+        </div>
+      </footer>
+  );
+};
 
 export default Footer
+
