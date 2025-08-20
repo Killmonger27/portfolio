390c392fde956d4955f2a445fc71b12f0e03c480
Killmonger27 <landryouarma45@gmail.com>
2025-08-20 17:10:33 +0000

    typo error fixed

--- app/projects/page.jsx
+++ app/projects/page.jsx
@@ -24,7 +24,7 @@ const Projects = () => {
     <section className="min-h-screen pt-12">
       <div className="container mx-auto">
         <h2 className="section-title mb-8 xl:mb-16 text-center mx-auto">
-          Mes Projets
+          My Projects
         </h2>
 
         <Tabs defaultValue={category} className="mb-24 xl:mb-48">
